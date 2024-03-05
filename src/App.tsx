77f6d9d49fd453e80b09341eaf0fbd19ec308f5d
jakerg@byu.edu <jakerg@byu.edu>
2024-03-05 10:16:14 -0700

    remove prettier change things to basketball

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,55 @@
 import React from 'react';
 import './App.css';
+import { JSX } from 'react/jsx-runtime';
 
-interface BandProps {
+interface BBallProps {
+  school: string;
   name: string;
-  members: string;
-  formed: number;
+  state: string;
+  city: string;
 }
 
-const bandNames = [
-  { name: 'The Beastie Boys', members: 'Ad Rock, MKA, Mike D', formed: 1979 },
-  { name: 'Wolfmother', members: 'Andrew, Bobby, Manish', formed: 2004 },
-  {
-    name: 'Cream',
-    members: 'Joseph, Eric Clappie, Jack Ruce, Ginger Baker',
-    formed: 1966,
-  },
-  { name: 'Nirvana', members: 'Juniper, Lilly, Lake Town', formed: 1909 },
-];
+const bandNames = require('./CollegeBasketballTeams.json');
 
 function Welcome() {
-  return <h1>The Best Music Trios Ever</h1>;
+  return <h1>List of Basketball Teams</h1>;
 }
 
 //This will print out one band at a time
-class Band extends React.Component<BandProps> {
+class BasketballTeam extends React.Component<BBallProps> {
   render() {
     //The properties are specified in the above interface (BandProps)
-    const oneBand = this.props;
+    const oneTeam = this.props;
 
     return (
       <div>
-        <img />
-        <h2>{oneBand.name}</h2>
-        <h2>{oneBand.members}</h2>
-        <h2>{oneBand.formed}</h2>
+        <h2>{oneTeam.school}</h2>
+        <h2>{oneTeam.name}</h2>
+        <h2>
+          {oneTeam.city}, {oneTeam.state}
+        </h2>
       </div>
     );
   }
 }
 
 //We use the map to loop through all the elements of the JSON object
-function BandList() {
+function BBallList() {
   return (
     <div>
       {/* This goes out to band names JSON
       and says I want to map one band at a time by
       going to the band component and grabbing the
       information for that one band. */}
-      {bandNames.map((oneBand) => (
-        <Band {...oneBand} />
-      ))}
+      {bandNames.map(
+        (
+          oneTeam: JSX.IntrinsicAttributes &
+            JSX.IntrinsicClassAttributes<BasketballTeam> &
+            Readonly<BBallProps>,
+        ) => (
+          <BasketballTeam {...oneTeam} />
+        ),
+      )}
     </div>
   );
 }
@@ -62,7 +62,7 @@ function App() {
       {/* In order to get the list of bands,
       we created another ocmponenet that is just a single band
       (see Band Class) */}
-      <BandList />
+      <BBallList />
     </div>
   );
 }
