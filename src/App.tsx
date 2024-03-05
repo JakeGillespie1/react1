01fa3df8c3c6ce5c86c32c37e8cc352d04ab0604
jakerg@byu.edu <jakerg@byu.edu>
2024-03-05 10:58:29 -0700

    Comments

--- src/App.tsx
+++ src/App.tsx
@@ -11,16 +11,18 @@ interface BBallProps {
   city: string;
 }
 
+//Make sure to grab the data in the correct format!
 const basketTeams = BasketballTeams.teams;
 
+//Welcome Message (Function/Component 1)
 function Welcome() {
   return <h1>List of Basketball Teams</h1>;
 }
 
-//This will print out one band at a time
+//This will print out one team at a time (Function/Component 2)
 class BasketballTeam extends React.Component<BBallProps> {
   render() {
-    //The properties are specified in the above interface (BandProps)
+    //The properties are specified in the above interface (BBallProps)
     const oneTeam = this.props;
 
     return (
@@ -37,6 +39,7 @@ class BasketballTeam extends React.Component<BBallProps> {
 }
 
 //We use the map to loop through all the elements of the JSON object
+//(Function/Component 3)
 function BBallList() {
   return (
     <div>
@@ -51,6 +54,7 @@ function BBallList() {
   );
 }
 
+//Button to display welcome message (Function/Component 4)
 function MyButton() {
   return (
     <button onClick={() => alert('Learn more about Basketball teams here!!')}>
@@ -67,9 +71,9 @@ function App() {
       <p>Click the button below to view a welcome message</p>
       <MyButton />
       <br />
-      {/* In order to get the list of bands,
-      we created another ocmponenet that is just a single band
-      (see Band Class) */}
+      {/* In order to get the list of basketball teams,
+      I created another component that is just a single team
+      (see BasketballTeam Class) */}
       <BBallList />
     </div>
   );
